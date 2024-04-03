438d947d7364644a329df37c4218295cc3403a40
AnnaLeeuni <125702515+AnnaLeeuni@users.noreply.github.com>
2024-04-03 18:58:31 -0400

    first commit

---
+++ Kanbas/Database/index.js
@@ -0,0 +1,7 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import assignment from "./assignment.js";
+import user from "./user.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+export default { courses, modules, assignment, user, grades, enrollments };
