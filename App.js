776ee750ab708a688386c4a7c4c68e10a75c5ec3
qingnanli <li.qingn@northeastern.edu>
2024-04-17 00:08:34 -0400

    Update App.js

--- App.js
+++ App.js
@@ -9,10 +9,11 @@ import cors from "cors";
 import UserRoutes from "./Users/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+const app = express()
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
 mongoose.connect(CONNECTION_STRING);
 
-const app = express()
+
 app.use(
     cors({
       credentials: true,
