0790d0297e603ab092e953c6e2cca5f4fad7a490
qingnanli <li.qingn@northeastern.edu>
2024-04-16 23:52:00 -0400

    final

---
+++ Kanbas/courses/dao.js
@@ -0,0 +1,11 @@
+import courseModel from "./model.js";
+
+export const findAllCourses = () => courseModel.find();
+export const findCourseById = (courseId) => courseModel.findOne({id:courseId});
+export const updateCourse = (courseId, course) =>  courseModel.updateOne({ _id: courseId }, { $set: course });
+export const deleteCourse = (courseId) => courseModel.deleteOne({ _id: courseId });
+export const createCourse = (course) => {
+    const id = course._id
+    delete course._id; 
+    return courseModel.create({...course, id});
+};
